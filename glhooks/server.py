098ef4e9fb168aab598637c432b859076b06967b
Blagovest Petrov <blagovest@petrovs.info>
2016-04-24 18:04:27 +0300

    some changes

--- glhooks/server.py
+++ glhooks/server.py
@@ -5,8 +5,6 @@ from __future__ import division, print_function, unicode_literals
 
 import json
 
-#from .mailer import HtmlMessage
-
 try:
     from http.server import BaseHTTPRequestHandler, HTTPServer
 except ImportError:
@@ -37,50 +35,27 @@ class GitlabWebhookHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         data_size = int(self.headers["Content-Length"])
         data = self.rfile.read(data_size)
-        json_data = json.loads(data.decode("utf-8"))
+        repo_url = "http://git.lo/blago/it-dns"
+        repo = Repository("http://git.lo/blago/it-dns.git")
+        repo.pull(branch="master")
+        #json_data = json.loads(data.decode("utf-8"))
 
-        try:
-            self.handle_commits_data(json_data)
-        except Exception as e:
-            self.context.logger.exception("Error during parsing of data", json_data)
-       #     self._send_email(json_data, e)
-            self._send_response_message(self.RESPONSE_MESSAGE, status_code=500)
-        else:
-            self._send_response_message(self.RESPONSE_MESSAGE)
+        #try:
+        #    self.handle_commits_data(json_data)
+        #except Exception as e:
+        #    self.context.logger.exception("Error during parsing of data", json_data)
+        #    self._send_response_message(self.RESPONSE_MESSAGE, status_code=500)
+        #else:
+        #    self._send_response_message(self.RESPONSE_MESSAGE)
 
-    def handle_commits_data(self, commits_json):
-        repo_url = commits_json["repository"]["homepage"]
-        repo_data = self.context.find_repo(repo_url)
-        if repo_data is None:
-            raise Exception("No configuration found for repository.", repo_url)
-
-        repo = Repository(repo_data["path"])
-        repo.pull(branch=repo_data.get("branch"))
-
-#    def _send_email(self, commits_json, exception):
-#        MESSAGE = """
-#        An error occured during GitLab webhook at server %(server.host)s.<br>
-#        <strong>%(exception)r</strong><br>
-#        Following JSON was received:<br><br>
-#        <pre><code>%(json)s</code></pre>
-#        """ % {
-#            "server.host": self.context["server"]["host"],
-#            "json": json.dumps(commits_json, indent=2),
-#            "exception": exception,
-#        }
+#    def handle_commits_data(self, commits_json):
+#        repo_url = commits_json["repository"]["homepage"]
+#        repo_data = self.context.find_repo(repo_url)
+#        if repo_data is None:
+#            raise Exception("No configuration found for repository.", repo_url)
 #
-#        emails = self._gather_emails(commits_json)
-#        message = HtmlMessage(self.context["mailer"]["sender"], "Deploy error", MESSAGE)
-#        message.add_recipients(*emails)
-#        self.context.mailer(message)
-
-    def _gather_emails(self, commits_json):
-        emails = set()
-        for commit in commits_json["commits"]:
-            emails.add(commit["author"]["email"])
-
-        emails.add(self.context["server"]["email"])
-        return list(emails)
+#        repo = Repository(repo_data["path"])
+#        repo.pull(branch=repo_data.get("branch"))
 
     def _send_response_message(self, message, status_code=200):
         message = message.encode("utf-8")
